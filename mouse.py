dbdcc24df213056b6a4c91aab0a3f089b923bfdb
Rahsal <rahsal1996@gmail.com>
2019-11-08 17:06:16 +0530

    Update mouse.py

--- mouse.py
+++ mouse.py
@@ -5,4 +5,4 @@ mouse = Controller()
 while True:
     a = list(mouse.position)
     mouse.position = (a[0]+1, a[1]+1)
-    time.sleep(10)
+    time.sleep(10)                      #for 10 seconds (change for adjust the time)
